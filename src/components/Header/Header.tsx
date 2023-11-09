daddb8ddfb03f0c266c85af93f56fcd1dd870f80
AlwastDev <sshapar13371@gmail.com>
2023-11-09 15:17:07 +0200

    mobile markup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ReactComponent as LogoIcon } from '../../images/logo.svg';
+import { ReactComponent as MenuIcon } from '../../images/icon-menu.svg';
 import { NavItem } from '../NavItem/NavItem';
 import { NavMenu } from '../NavMenu/NavMenu';
 import { COMPANIES, FEATURES } from './constant.js';
@@ -9,19 +10,22 @@ const Header = () => {
   return (
     <header className='flex items-center'>
       <LogoIcon />
-      <nav className='flex space-x-6 ml-8 items-center'>
+      <nav className='hidden xl:flex space-x-6 ml-8 items-center'>
         <NavItem text='Feature'>
           <NavMenu items={FEATURES} />
         </NavItem>
         <NavItem text='Company'>
           <NavMenu items={COMPANIES} />
         </NavItem>
-        <NavItem text='Careers'></NavItem>
-        <NavItem text='About'></NavItem>
+        <NavItem text='Careers' />
+        <NavItem text='About' />
       </nav>
-      <div className='ml-auto flex space-x-5'>
+      <div className='hidden ml-auto xl:flex space-x-5'>
         <Button>Login</Button>
         <Button hasBorder={true}>Register</Button>
+      </div>
+      <div className='xl:hidden flex ml-auto cursor-pointer z-999'>
+        <MenuIcon />
       </div>
     </header>
   );
--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -4,26 +4,30 @@ import { ReactComponent as Audiophile } from '../../images/client-audiophile.svg
 import { ReactComponent as Meet } from '../../images/client-meet.svg';
 import { ReactComponent as Maker } from '../../images/client-maker.svg';
 import MenImage from '../../images/image-hero-desktop.png';
+import MenMobileImage from '../../images/image-hero-mobile.png';
 import { Button } from '../Button/Button';
 
 const MainSection = () => {
   return (
-    <section className='w-full flex mt-6 h-screen justify-between'>
-      <div className='relative w-2/4 text-left mt-12'>
-        <h1 className='text-8xl font-black mt-60 whitespace-pre-line'>{`Make\n remote work`}</h1>
+    <section className='w-full flex-col xl:flex-row flex mt-6 xl:h-screen justify-between'>
+      <div className='relative order-2 xl:order-1 text-center xl:text-left mt-12 xl:mt-60'>
+        <h1 className='text-3xl xl:text-8xl font-black xl:whitespace-pre-line mt-6'>{`Make\n remote work`}</h1>
         <p className='text-medium-gray text-lg my-12 whitespace-pre-line'>
           {`Lorem ipsum dolor, sit amet consectetur adipisicing elit.\n Amet ullam dolorum animi atque vitae exercitationem\n quibusdam maiores adipisci qui harum.`}
         </p>
         <Button isFilled={true}>Learn More</Button>
-        <div className='flex justify-around absolute bottom-2 w-full'>
+        <div className='flex justify-around xl:absolute bottom-2 w-full mt-20'>
           <Databiz />
           <Audiophile />
           <Meet />
           <Maker />
         </div>
       </div>
-      <div className='flex w-2/4 mt-20'>
+      <div className='hidden xl:flex w-2/4 mt-20 xl:order-2'>
         <img src={MenImage} alt='men' />
+      </div>
+      <div className='flex xl:hidden w-full mt-20 justify-center mb-8 order-1'>
+        <img src={MenMobileImage} alt='men' />
       </div>
     </section>
   );
